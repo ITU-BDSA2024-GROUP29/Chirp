a9a435645e7e9b2558116421fad33f148f28d85d
Magnus Rolin <magnus.thor@live.dk>
2024-09-03 20:50:01 +0200

    Base CLI Setup
    - Environment setup
    - Vs-code debug external runtime added
    - Converted the standard internal console into external console
    
    Co-Authored-By: yeeerdley-itu <145338315+yeeerdley-itu@users.noreply.github.com>
    Co-Authored-By: Lukas <56886866+ItsLukV@users.noreply.github.com>

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Chirp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+
+
+    }
+}
