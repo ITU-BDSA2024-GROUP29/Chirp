7fd8fc9be67fd7b965752a6254cf72bd1662a746
yeeerdley-itu <145338315+yeeerdley-itu@users.noreply.github.com>
2024-09-08 15:39:02 +0200

    installed docopt.net and made first iteration of a command-line interface

--- Program.cs
+++ Program.cs
@@ -1,5 +1,20 @@
 ﻿using System.Text.RegularExpressions;
 using SimpleDB;
+using DocoptNet;
+
+const string usage = @"Chirp CLI version.
+
+Usage: 
+    chirp read <limit>
+    chirp cheep <message>
+    chirp (-h | --help)
+    chirp --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+";
+var arguments = new Docopt().Apply(usage, args, version: "1.0", exit: true)!;
 
 namespace Chirp
 {
