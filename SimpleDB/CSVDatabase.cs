b89b79b45a13f7f9769da578dda9b2df72996c4f
Magnus Rolin <69061700+MRollin03@users.noreply.github.com>
2024-09-25 15:19:58 +0200

    Update CSVDatabase.cs
    - convert to singleton
    
    Co-Authored-By: yeeerdley-itu <145338315+yeeerdley-itu@users.noreply.github.com>

--- SimpleDB/CSVDatabase.cs
+++ SimpleDB/CSVDatabase.cs
@@ -1,22 +1,19 @@
 using SimpleDB;
 using CsvHelper;
 using System.Globalization;
-using Chirp;
-using System.Collections.Generic;
-using System.Linq;
 
 
 public record Cheep(String Author, String Message, long TimeStamp);
 public class CSVDatabase<T> : IDatabaseRepository<T>
 {
-    string FilePath;
-    public CSVDatabase(string FilePath){
-        this.FilePath = FilePath;
+    public static CSVDatabase<Cheep> csvdatabase;
+    public CSVDatabase(){
+
     }
      public IEnumerable<T> Read(int? limit){
         List<Cheep> ListCheep = new List<Cheep>();
         int i = 0;
-        using (var reader = new StreamReader(FilePath))
+        using (var reader = new StreamReader("Data/cheeps.csv"))
         using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
         {
             var cheeps = csv.GetRecords<Cheep>();
@@ -24,7 +21,7 @@ public class CSVDatabase<T> : IDatabaseRepository<T>
             foreach(Cheep cheep in cheeps){
                 ListCheep.Add(cheep);
                 i++;
-                if(i == limit && i != 0){       //limit = 0 is for print all Cheeps
+                if(i == limit){
                     break;
                 }
             }
@@ -32,7 +29,7 @@ public class CSVDatabase<T> : IDatabaseRepository<T>
         return ListCheep.Cast<T>();
      }
     public void Store(T record){
-        using (var Stream = File.Open(FilePath,FileMode.Append))
+        using (var Stream = File.Open("Data/cheeps.csv",FileMode.Append))
         using (var writer = new StreamWriter(Stream))
         using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
         {
@@ -43,7 +40,8 @@ public class CSVDatabase<T> : IDatabaseRepository<T>
         Console.WriteLine("Cheep was succesfully sent!");
     }
 
-    public static CSVDatabase<Cheep> GetDatabase(string FilePath){
-        return new CSVDatabase<Cheep>(FilePath);
+    public static CSVDatabase<Cheep> GetInstance()
+    {
+        return csvdatabase ??= new CSVDatabase<Cheep>(); 
     }
 }
