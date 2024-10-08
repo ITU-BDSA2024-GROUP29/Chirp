117dde73a9fd859983a3ffc2d5a8055c94121334
yeeerdley-itu <145338315+yeeerdley-itu@users.noreply.github.com>
2024-10-08 13:51:11 +0200

    made initial db schema

--- src/Chirp.Razor/DomainModel/CheepDBContext.cs
+++ src/Chirp.Razor/DomainModel/CheepDBContext.cs
@@ -8,18 +8,6 @@ public class CheepDBContext : DbContext {
     public CheepDBContext(DbContextOptions<CheepDBContext> options) : base(options) {
         Cheeps = Set<Cheep>();
         Users = Set<User>();
-        test();
-    }
-    
-    
-    public async void test() {
-        
-        // Define the query - with our setup, EF Core translates this to an SQLite query in the background
-        var query = from Cheep in Cheeps
-            where Cheep.User.Name == "Adrian"
-            select new { Cheep.Text, Cheep.User };
-        // Execute the query and store the results
-        var result = await query.ToListAsync();
     }
 
 }
