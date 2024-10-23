3caf80e7c73bb686fc9b489d50867218c7fde608
Sweatyfish <106493731+Sweatyfish@users.noreply.github.com>
2024-10-23 13:38:43 +0200

    Fixed our ChirpRazorTest so that now i works and created the first test on our CheepReposity
    Co-authored-by: Rasmus <rahu@itu.dk>
    Co-authored-by: Viktor <viea@itu.dk>

--- src/Chirp.Razor/CheepRepository/ICheepRepository.cs
+++ src/Chirp.Razor/CheepRepository/ICheepRepository.cs
@@ -4,7 +4,7 @@ namespace Chirp.Razor.CheepRepository;
 
 public interface ICheepRepository {
     Task CreateCheepAsync(Cheep newCheep);
-    Task<List<Cheep>> ReadCheeps(String userName);
+    Task<List<Cheep>> ReadCheeps();
     Task<List<Cheep>> GetPaginatedCheeps(int pageNumber, int pageSize);
     //Task UpdateCheep(Cheep alteredcheep);
 
---
+++ test/ChirpRazor.Tests/CheepepositoryTest.cs
@@ -0,0 +1,48 @@
+namespace Chirp.Razor.test.ChirpRazor.Tests;
+
+using System.Threading.Tasks;
+using Chirp.Razor.CheepRepository;
+using Chirp.Razor.DomainModel;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+
+
+public class Test(){
+
+    [Fact]
+    public async Task testCheepRepository(){
+        //Arange
+        using var connection = new SqliteConnection("Filename=:memory:");
+        await connection.OpenAsync();
+        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+
+        using var context = new ChirpDBContext(builder.Options);
+        await context.Database.EnsureCreatedAsync();
+
+        ICheepRepository cheepRepository = new CheepRepository(context);
+
+        var userName = "Helge";
+        var TimeStamp = "8/1/2023 12:16:48 PM";
+        var Message = "Hello, BDSA students!";
+        var CheepId = 656;
+        var AuthorId = 11;
+
+
+        var list = cheepRepository.ReadCheeps().Result;
+        
+        var result = list[list.Count - 2];
+
+        
+        
+        
+        Assert.True(result.CheepId == CheepId);
+        Assert.True(result.AuthorId == AuthorId);
+        Assert.True(result.Author.Name == userName);
+        Assert.True(result.Text == Message);
+        Assert.True(result.TimeStamp.ToString()== TimeStamp);
+
+
+    }
+}
