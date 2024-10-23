ed454c44eca6ea313f73794e8381b87d5575d347
Magnus Rolin <69061700+MRollin03@users.noreply.github.com>
2024-10-23 12:00:28 +0200

    Pagenation added
    - pages
    - and reversed message apperence

--- src/Chirp.Razor/CheepRepository/ICheepRepository.cs
+++ src/Chirp.Razor/CheepRepository/ICheepRepository.cs
@@ -5,9 +5,8 @@ namespace Chirp.Razor.CheepRepository;
 public interface ICheepRepository {
     Task CreateCheepAsync(Cheep newCheep);
     Task<List<Cheep>> ReadCheeps(String userName);
-    
     Task<List<Cheep>> GetPaginatedCheeps(int pageNumber, int pageSize);
-    Task UpdateCheep(Cheep alteredcheep);
+    //Task UpdateCheep(Cheep alteredcheep);
 
     
 }
