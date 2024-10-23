a57aa3bc536c7a4bd1c7509fd3f4c1cc6f2a681e
Lukas <56886866+ItsLukV@users.noreply.github.com>
2024-10-23 14:08:59 +0200

    added timestamp

--- test/ChirpRazor.Tests/CheepepositoryTest.cs
+++ test/ChirpRazor.Tests/CheepepositoryTest.cs
@@ -25,6 +25,7 @@ public class Test(){
 
         var userName = "Helge";
         var Message = "Hello, BDSA students!";
+        var TimeStamp = "01-08-2023 12:16:48"
         var CheepId = 656;
         var AuthorId = 11;
 
@@ -39,6 +40,7 @@ public class Test(){
         Assert.True(result.AuthorId == AuthorId);
         Assert.True(result.Author.Name == userName);
         Assert.True(result.Text == Message);
+        Assert.True(result.TimeStamp.ToString("dd-MM-yyyy HH:mm:ss")== TimeStamp);
 
 
     }
