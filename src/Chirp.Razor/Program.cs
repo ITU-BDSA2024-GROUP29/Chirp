c67de7626c23c4f593a2462735ea8bf3dc68568e
Lukas <lukasvranic@gmail.com>
2024-11-06 14:45:31 +0100

    undo of https

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -38,7 +38,7 @@ namespace Chirp.Razor {
 
             // Build
             // Once you are sure everything works, you might want to increase this value to up to 1 or 2 years
-            builder.Services.AddHsts(options => options.MaxAge = TimeSpan.FromHours(1));
+            // builder.Services.AddHsts(options => options.MaxAge = TimeSpan.FromHours(1));
             var app = builder.Build();
             _ = app.Services.GetService<IServiceCollection>(); //delete??
 
@@ -58,9 +58,10 @@ namespace Chirp.Razor {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
-            } else if(app.Environment.IsProduction()) {
-                app.UseHsts(); // Send HSTS headers, but only in production
             }
+            // else if(app.Environment.IsProduction()) {
+            //     app.UseHsts(); // Send HSTS headers, but only in production
+            // }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
