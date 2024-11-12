73c4efebdc137f73372ab6259607be98f3b889e9
Viea2000 <viea@itu.dk>
2024-11-12 13:05:53 +0100

    plz work?
    123

--- src/Chirp.Razor/Program.cs
+++ src/Chirp.Razor/Program.cs
@@ -6,21 +6,30 @@ using AspNet.Security.OAuth.GitHub;
 using Microsoft.AspNetCore.Authentication.Cookies;
 
 
-namespace Chirp.Razor {
+namespace Chirp.Razor
+{
 
-    class Program {
-        public static void Main(String[] args) {
+    class Program
+    {
+        public static void Main(String[] args)
+        {
             var builder = WebApplication.CreateBuilder(args);
 
             // Load database connection via configuration
-            string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-            builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
+            builder.Services.AddDbContext<ChirpDBContext>(options =>
+                options.UseSqlite(builder.Configuration
+                .GetConnectionString("DefaultConnection")));
+
+            builder.Services.AddDefaultIdentity<ApplicationUser>(options => 
+            options.SignIn.RequireConfirmedAccount = false) 
+            .AddEntityFrameworkStores<ChirpDBContext>(); 
 
             // Add services to the container.
             builder.Services.Core();
             builder.Services.AddRazorPages();
             builder.Services.AddScoped<ICheepRepository, CheepRepository>();
             builder.Services.AddScoped<ICheepService, CheepService>();
+
 
             // builder.Services.AddAuthentication(options =>
             // {
@@ -37,27 +46,41 @@ namespace Chirp.Razor {
             // });
 
             // Build
+            // Once you are sure everything works, you might want to increase this value to up to 1 or 2 years
+            // builder.Services.AddHsts(options => options.MaxAge = TimeSpan.FromHours(1));
             var app = builder.Build();
             _ = app.Services.GetService<IServiceCollection>(); //delete??
 
 
-            using (var serviceScope = app.Services.CreateScope()) {
+
+
+
+
+            using (var serviceScope = app.Services.CreateScope())
+            {
                 var services = serviceScope.ServiceProvider;
                 var context = services.GetRequiredService<ChirpDBContext>();
                 DbInitializer.SeedDatabase(context);
             }
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment()) {
+            if (!app.Environment.IsDevelopment())
+            {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                // app.UseHsts();
             }
+            // else if(app.Environment.IsProduction()) {
+            //     app.UseHsts(); // Send HSTS headers, but only in production
+            // }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.MapRazorPages();
 
--- test/ChirpRazor.Tests/CheepServiceTest.cs
+++ test/ChirpRazor.Tests/CheepServiceTest.cs
@@ -8,139 +8,141 @@ namespace Chirp.Razor.test.ChirpRazor.Tests
     public class CheepServiceTest(){
     [Fact]
     public async Task Test_CheepServiceDependencyInjection_NotNull_NotEmpty(){
-        using var connection = new SqliteConnection("Filename=:memory:");
-        await connection.OpenAsync();
-        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+        using (var connection = new SqliteConnection("Filename=:memory:")){
+            await connection.OpenAsync();
+            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
 
-        using var context = new ChirpDBContext(builder.Options);
-        await context.Database.EnsureCreatedAsync();
-        DbInitializer.SeedDatabase(context);
+            using (var context = new ChirpDBContext(builder.Options)){
+                 await context.Database.EnsureCreatedAsync();
+                DbInitializer.SeedDatabase(context);
 
-        ICheepRepository cheepRepository = new CheepRepository(context);
-        
+                ICheepRepository cheepRepository = new CheepRepository(context);
+                
 
-        ICheepService service = CheepService.GetInstance(cheepRepository);
+                ICheepService service = CheepService.GetInstance(cheepRepository);
 
-        var list = service.GetCheepsAsync().Result;
+                var list = await service.GetCheepsAsync();
 
-        Assert.False(list.Count == 0);
+                Assert.False(list.Count == 0);
 
-        Assert.Same(cheepRepository, service.GetCheepRepository());
+                Assert.Same(cheepRepository, service.GetCheepRepository());
 
-        connection.Dispose();
+            }
+        }
     }
 [Fact]
     public async Task Test_CheepCount_Equal(){
-        using var connection = new SqliteConnection("Filename=:memory:");
-        await connection.OpenAsync();
-        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+        using (var connection = new SqliteConnection("Filename=:memory:")){
+            await connection.OpenAsync();
+            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
 
-        using var context = new ChirpDBContext(builder.Options);
-        await context.Database.EnsureCreatedAsync();
-        DbInitializer.SeedDatabase(context);
+            using (var context = new ChirpDBContext(builder.Options)){
+                await context.Database.EnsureCreatedAsync();
+                DbInitializer.SeedDatabase(context);
 
-        CheepRepository cheepRepository = new CheepRepository(context);
-        
+                CheepRepository cheepRepository = new CheepRepository(context);
+                
 
-        ICheepService service = new CheepService(cheepRepository);
+                ICheepService service = new CheepService(cheepRepository);
 
-        var list = service.GetCheepsAsync().Result;
+                var list = await service.GetCheepsAsync();
 
-        
-        Assert.Equal(service.GetTotalCheepCount().Result, list.Count);
+                
+                Assert.Equal(await service.GetTotalCheepCount(), list.Count);
 
-        connection.Dispose();
+            }
+        }
+
     }
 
         [Fact]
     public async Task Test_GetCheeps_IdenticalCheep(){
-        using var connection = new SqliteConnection("Filename=:memory:");
-        await connection.OpenAsync();
-        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+        using (var connection = new SqliteConnection("Filename=:memory:")){
+            await connection.OpenAsync();
+            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
 
-        using var context = new ChirpDBContext(builder.Options);
-        await context.Database.EnsureCreatedAsync();
-        DbInitializer.SeedDatabase(context);
+            using (var context = new ChirpDBContext(builder.Options)){
+                await context.Database.EnsureCreatedAsync();
+                DbInitializer.SeedDatabase(context);
 
-        CheepRepository cheepRepository = new CheepRepository(context);
-        
+                CheepRepository cheepRepository = new CheepRepository(context);
+                
 
-        ICheepService service = new CheepService(cheepRepository);
+                ICheepService service = new CheepService(cheepRepository);
 
-        var list = service.GetCheepsAsync().Result;
+                var list = await service.GetCheepsAsync();
 
-        var userName = "Helge";
-        var Message = "Hello, BDSA students!";
-        var TimeStamp = "08/01/2023 12:16:48";
+                var userName = "Helge";
+                var Message = "Hello, BDSA students!";
 
-        Assert.Equal(userName,list[list.Count-1].Author);
-        Assert.Equal(Message,list[list.Count-1].Message);
-        
-        connection.Dispose();
+                Assert.Equal(userName,list[list.Count-1].Author);
+                Assert.Equal(Message,list[list.Count-1].Message);
+
+            }
+        }
     }
 
     [Fact]
     public async Task Test_getCheepsByAuthor_IdenticalCheep(){
-         using var connection = new SqliteConnection("Filename=:memory:");
-        await connection.OpenAsync();
-        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+         using (var connection = new SqliteConnection("Filename=:memory:")){
+            await connection.OpenAsync();
+            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
 
-        using var context = new ChirpDBContext(builder.Options);
-        await context.Database.EnsureCreatedAsync();
-        DbInitializer.SeedDatabase(context);
+            using (var context = new ChirpDBContext(builder.Options)){
+                 await context.Database.EnsureCreatedAsync();
+                DbInitializer.SeedDatabase(context);
 
-        CheepRepository cheepRepository = new CheepRepository(context);
-        
+                CheepRepository cheepRepository = new CheepRepository(context);
+                
 
-        ICheepService service = new CheepService(cheepRepository);
+                ICheepService service = new CheepService(cheepRepository);
 
-        var list = service.GetCheepsFromAuthorAsync("Helge").Result;
+                var list = await service.GetCheepsFromAuthorAsync("Helge");
 
-        var userName = "Helge";
-        var Message = "Hello, BDSA students!";
-        var TimeStamp = "01/08/2023 12:16:48";
+                var userName = "Helge";
+                var Message = "Hello, BDSA students!";
 
-        Assert.Equal(userName,list[0].Author);
-        Assert.Equal(Message,list[0].Message);
-        
-        connection.Dispose();
+                Assert.Equal(userName,list[0].Author);
+                Assert.Equal(Message,list[0].Message);
+            }
+         }
     }
  [Fact]
     public async Task Test_PaginatedPage_32Cheeps(){
-         using var connection = new SqliteConnection("Filename=:memory:");
-        await connection.OpenAsync();
-        var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
+         using (var connection = new SqliteConnection("Filename=:memory:")){
+            await connection.OpenAsync();
+            var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
 
-        using var context = new ChirpDBContext(builder.Options);
-        await context.Database.EnsureCreatedAsync();
-        DbInitializer.SeedDatabase(context);
+            using (var context = new ChirpDBContext(builder.Options)){
+                await context.Database.EnsureCreatedAsync();
+                DbInitializer.SeedDatabase(context);
 
-        CheepRepository cheepRepository = new CheepRepository(context);
-        
+                CheepRepository cheepRepository = new CheepRepository(context);
+                
 
-        ICheepService service = new CheepService(cheepRepository);
+                ICheepService service = new CheepService(cheepRepository);
 
-        
+                
 
-        const int PageSize = 32;
-        var totalCheeps = await service.GetTotalCheepCount();
-        var TotalPages = (int)Math.Ceiling(totalCheeps / (double)PageSize);
+                const int PageSize = 32;
+                var totalCheeps = await service.GetTotalCheepCount();
+                var TotalPages = (int)Math.Ceiling(totalCheeps / (double)PageSize);
 
-        var list = service.GetPaginatedCheepsAsync(TotalPages,PageSize).Result;
-        
+                var list = await service.GetPaginatedCheepsAsync(TotalPages,PageSize);
+                
 
-        var userName = "Helge";
-        var Message = "Hello, BDSA students!";
-        var TimeStamp = "01/08/2023 12:16:48";
+                var userName = "Helge";
+                var Message = "Hello, BDSA students!";
 
-        Assert.Equal(userName,list[list.Count-1].Author);
-        Assert.Equal(Message,list[list.Count-1].Message);
-        
+                Assert.Equal(userName,list[list.Count-1].Author);
+                Assert.Equal(Message,list[list.Count-1].Message);
+                
 
-        list = service.GetPaginatedCheepsAsync(1,PageSize).Result;
-        Assert.Equal(32,list.Count);
+                list = await service.GetPaginatedCheepsAsync(1,PageSize);
+                Assert.Equal(32,list.Count);
 
-        connection.Dispose();
+            }
+         }
     }
 }
 }
