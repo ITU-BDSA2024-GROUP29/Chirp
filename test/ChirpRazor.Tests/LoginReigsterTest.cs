4a260f2b019800a726aa3eaff007de1c0fb16cba
Sweatyfish <106493731+Sweatyfish@users.noreply.github.com>
2024-11-27 13:14:12 +0100

    Fixed Formatting
    Co-Authored-by:Rasmus <Rahu@itu.dk>

--- test/ChirpRazor.Tests/LoginReigsterTest.cs
+++ test/ChirpRazor.Tests/LoginReigsterTest.cs
@@ -9,26 +9,30 @@ using Microsoft.Extensions.Logging;
 namespace Chirp.Razor.test.ChirpRazor.Tests
 {
 
-    public class LoginRegisterTest{
+    public class LoginRegisterTest
+    {
 
 
         //This Test makes sure that our login system can actually create a user
         [Fact]
-        public async Task Test_CreateUser_True_NotNull(){
-            using (var connection = new SqliteConnection("Filename=:memory:")){
+        public async Task Test_CreateUser_True_NotNull()
+        {
+            using (var connection = new SqliteConnection("Filename=:memory:"))
+            {
                 await connection.OpenAsync();
                 var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
 
-                using (var context = new ChirpDBContext(builder.Options)){
+                using (var context = new ChirpDBContext(builder.Options))
+                {
                     await context.Database.EnsureCreatedAsync();
 
                     var userStore = new UserStore<ApplicationUser>(context);
 
 
-                    #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
                     var userManager = new UserManager<ApplicationUser>(
                     userStore, null, new PasswordHasher<ApplicationUser>(), null, null, null, null, null, null);
-                    #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
 
                     var user = new ApplicationUser { UserName = "TestUser", Email = "testuser@example.com" };
                     var result = await userManager.CreateAsync(user, "TestPassword123!");
@@ -43,22 +47,25 @@ namespace Chirp.Razor.test.ChirpRazor.Tests
 
         //this methods test the login in credentials using the objects our .identity migration gave
         [Fact]
-        public async Task Test_Logincredentialstest_false_true(){
-                using (var connection = new SqliteConnection("Filename=:memory:")){
+        public async Task Test_Logincredentialstest_false_true()
+        {
+            using (var connection = new SqliteConnection("Filename=:memory:"))
+            {
                 await connection.OpenAsync();
                 var builder = new DbContextOptionsBuilder<ChirpDBContext>().UseSqlite(connection);
 
-                using (var context = new ChirpDBContext(builder.Options)){
+                using (var context = new ChirpDBContext(builder.Options))
+                {
                     await context.Database.EnsureCreatedAsync();
 
                     var userStore = new UserStore<ApplicationUser>(context);
                     var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole());  // You can add other log providers like File, etc.
                     var logger = loggerFactory.CreateLogger<UserManager<ApplicationUser>>();
 
-                    #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
                     var userManager = new UserManager<ApplicationUser>(
                     userStore, null, new PasswordHasher<ApplicationUser>(), null, null, null, null, null, logger);
-                    #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
 
                     var user = new ApplicationUser { UserName = "TestUser", Email = "testuser@example.com" };
                     var result = await userManager.CreateAsync(user, "TestPassword123!");
@@ -68,11 +75,11 @@ namespace Chirp.Razor.test.ChirpRazor.Tests
 
                     await context.SaveChangesAsync();
 
-                    
-                    var loginTry = await userManager.CheckPasswordAsync(user,"WrongPassword!");
+
+                    var loginTry = await userManager.CheckPasswordAsync(user, "WrongPassword!");
                     Assert.False(loginTry);
 
-                    loginTry = await userManager.CheckPasswordAsync(user,"TestPassword123!");
+                    loginTry = await userManager.CheckPasswordAsync(user, "TestPassword123!");
                     Assert.True(loginTry);
 
 
